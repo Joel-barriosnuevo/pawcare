0f108faf91a4639d3914c6db0da1e9d8f86a5b92
Joel-barriosnuevo <joelbarriosnuevo123@gmail.com>
2025-03-28 17:17:55 -0500

    Add GitHub Pages configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',  // Necesario para GitHub Pages
+  basePath: '/pawcare', // Nombre del repositorio
+  images: {
+    unoptimized: true, // Necesario para export estático
+  },
+}
 
 export default nextConfig;
